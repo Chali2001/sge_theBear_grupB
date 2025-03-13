5c5bdbb206aa7c97f4a99f38652923039207ea11
dev <dev@introprg.myguest.virtualbox.org>
2025-03-13 21:14:11 +0100

    añadido y terminado 'user','models'

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 from typing import List
-from fastapi import FastAPI
-from services import read
+from fastapi import FastAPI, Depends
+from services import read, user
+from dotenv import load_dotenv
+from sqlmodel import SQLModel, create_engine, Session
+import os
 
 app = FastAPI()
 
-@app.get("/root", response_model=List[dict])
-async def read_root():
-    result = read.registre()
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
+SQLModel.metadata.create_all(engine)
+
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/users/", response_model= list[dict])
+def read_user(db:Session = Depends(get_db)):
+    result = user.get_all_users(db)
     return result
+
+@app.get("/users/", response_model=dict)
+def create_user(name: str, email:str, db:Session = Depends(get_db)):
+    result = user.add_new_user(name, email, db)
+    return result
+

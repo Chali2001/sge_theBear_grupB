50760f03c585087862fdf955700a44cb6a2f6422
Baorii3 <baorii310@gmail.com>
2025-05-07 10:51:59 +0200

    canvio de nada

--- models/Punto_Venta.py
+++ models/Punto_Venta.py
@@ -2,11 +2,11 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 from models import Factura
 from models import Pedido
+from typing import Optional
 
 class Punto_Venta(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     id_pedido: int
     reserva: bool
-    id_factura: int
-
+    id_factura: Optional[int] = Field(default=None, foreign_key="factura.id")
 
